80642daefafc2875e2d9fd8be75aabc2797be10c
amirhossein <79211332+capamir@users.noreply.github.com>
2025-12-09 23:15:17 +0330

    project started

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <div className="p-8">TODO: Portfolio layout</div>
+    </main>
+  );
+}
